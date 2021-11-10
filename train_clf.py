979380d59bc0ad8398e366c2940d13ede90b7ff4
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2021-11-10 14:22:13 +0530

    Update train_clf.py

--- train_clf.py
+++ train_clf.py
@@ -41,9 +41,6 @@ if __name__ == "__main__":
     train_y = train[["Yearly Amount Spent"]]
     test_y = test[["Yearly Amount Spent"]]
 
-    #alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    #l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
     with mlflow.start_run():
         
         lr = ElasticNet()
@@ -59,8 +56,6 @@ if __name__ == "__main__":
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
-        #mlflow.log_param("alpha", alpha)
-        #mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
